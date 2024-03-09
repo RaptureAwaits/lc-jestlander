baae3ae36d0d3fbf4ccb00092dc8bd8a56d494e8
Frederic Smythe <fredericsmythe512@gmail.com>
2024-03-09 17:35:31 +0000

    Added separate behaviour for host and non-host players to address non-hosts having no access to true windup timer

--- LCJestlander/Plugin.cs
+++ LCJestlander/Plugin.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Reflection;
 using HarmonyLib;
 using UnityEngine;
-using System;
 using BepInEx.Logging;
 
 namespace LCJestlander {
@@ -11,15 +10,15 @@ namespace LCJestlander {
     public class JestlanderBase : BaseUnityPlugin {
 		private const string mod_guid = "raptureawaits.jestlander";
 		private const string mod_name = "Jestlander";
-		private const string mod_version = "1.1.0";
+		private const string mod_version = "1.2.0";
 
 		internal static JestlanderBase instance;
 		internal static ManualLogSource modlog;
 		
 		public static AssetBundle new_sounds;
-		internal static AudioClip[] windup_audio;
-		internal static AudioClip[] pop_audio;
-		internal static AudioClip[] scream_audio;
+		internal static AudioClip[] windup_asset;
+		internal static AudioClip[] pop_asset;
+		internal static AudioClip[] scream_asset;
 		
 		private readonly Harmony harmony = new(mod_guid);
 		
@@ -36,9 +35,9 @@ namespace LCJestlander {
 				return;
 			}
 			
-			windup_audio = new_sounds.LoadAssetWithSubAssets<AudioClip>("assets\\audio\\windup.wav");
-			pop_audio = new_sounds.LoadAssetWithSubAssets<AudioClip>("assets\\audio\\pop.wav");
-			scream_audio = new_sounds.LoadAssetWithSubAssets<AudioClip>("assets\\audio\\scream.wav");
+			windup_asset = new_sounds.LoadAssetWithSubAssets<AudioClip>("assets\\audio\\windup.wav");
+			pop_asset = new_sounds.LoadAssetWithSubAssets<AudioClip>("assets\\audio\\pop.wav");
+			scream_asset = new_sounds.LoadAssetWithSubAssets<AudioClip>("assets\\audio\\scream.wav");
 			
 			harmony.PatchAll(typeof(Patches.JesterAIPatch));
             modlog.LogInfo($"Plugin {mod_guid} is loaded!");
@@ -50,21 +49,22 @@ namespace LCJestlander.Patches {
 	[HarmonyPatch(typeof(JesterAI))]
 	internal class JesterAIPatch {
 		internal static ManualLogSource modlog = JestlanderBase.modlog;
+		internal static float second_offset;
 
 		[HarmonyPatch("Start")]
 		[HarmonyPrefix]
 		static void AudioPatch(ref AudioClip ___popUpSFX, ref AudioClip ___screamingSFX) {
-			AudioClip[] pop_replace = JestlanderBase.pop_audio;
-			if (pop_replace != null && pop_replace.Length > 0) {
-				AudioClip _ = pop_replace[0];
+			AudioClip[] pop_list = JestlanderBase.pop_asset;
+			if (pop_list != null && pop_list.Length > 0) {
+				AudioClip _ = pop_list[0];
 				___popUpSFX = _;
 			} else {
 				modlog.LogError("Failed to load pop replacement audio data from extracted assets.");
 			}
 
-			AudioClip[] scream_replace = JestlanderBase.scream_audio;
-			if (scream_replace != null && scream_replace.Length > 0) {
-				AudioClip _ = scream_replace[0];
+			AudioClip[] scream_list = JestlanderBase.scream_asset;
+			if (scream_list != null && scream_list.Length > 0) {
+				AudioClip _ = scream_list[0];
 				___screamingSFX = _;
 			} else {
 				modlog.LogError("Failed to load scream replacement audio data from extracted assets.");
@@ -74,23 +74,26 @@ namespace LCJestlander.Patches {
 		[HarmonyPatch("SetJesterInitialValues")]
 		[HarmonyPostfix]
 		static void AudioPatch(ref AudioClip ___popGoesTheWeaselTheme, ref float ___popUpTimer) {
-			AudioClip[] windup_replace = JestlanderBase.windup_audio;
-			if (windup_replace != null && windup_replace.Length > 0) {
-				AudioClip windup_clip = windup_replace[0];
+			AudioClip[] windup_list = JestlanderBase.windup_asset;
+			if (windup_list != null && windup_list.Length > 0) {
+				AudioClip windup_clip = windup_list[0];
 
-				float second_offset = windup_clip.length - 5f - ___popUpTimer;
+				if (GameNetworkManager.Instance.isHostingGame) {
+					modlog.LogInfo("Player is host, adjusting windup clip...");
+					second_offset = windup_clip.length - 5f - ___popUpTimer;
+				} else {
+					modlog.LogInfo("Player is not host, using static windup clip.");
+					second_offset = windup_clip.length - 40f;
+				}
+
 				int sample_offset = (int)(second_offset / windup_clip.length * windup_clip.samples);
-
 				int trim_samples = windup_clip.samples - sample_offset;
 				float[] samples = new float[trim_samples * windup_clip.channels];
 				windup_clip.GetData(samples, sample_offset);
+				AudioClip new_clip = AudioClip.Create(windup_clip.name + "_cropped", trim_samples, windup_clip.channels, windup_clip.frequency, false);
+				new_clip.SetData(samples, 0);
 
-				
-				AudioClip cropped_windup = AudioClip.Create(windup_clip.name + "_cropped", trim_samples, windup_clip.channels, windup_clip.frequency, false);
-				cropped_windup.SetData(samples, 0);
-				
-				___popGoesTheWeaselTheme = cropped_windup;
-				modlog.LogInfo($"Windup length: {___popUpTimer}, clip offset: {second_offset}, clip length: {cropped_windup.length}");
+				___popGoesTheWeaselTheme = new_clip;
 			} else {
 				modlog.LogError("Failed to load windup replacement audio data from extracted assets.");
 			}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # lc-jestlander
-Sound replacement mod for hit video game Lethal Company. Replaces Jester windup and scream noises with *that bit* from hit song Highlander by Lost Horizon. Clip lengths are dynamically adjusted for a near seamless transition.
+Sound replacement mod for hit video game Lethal Company. Replaces Jester windup and scream noises with *that bit* from hit song Highlander by Lost Horizon. Clips are altered and dynamically adusted (for host players only) to make the transition between clips as smooth as possible.
